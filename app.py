2b9c3782ec3aa288f5ac6e987693785fad828172
Ubuntu <fake@code-runner.c5ll5ipqlowexmadjbpirzpo2g.bx.internal.cloudapp.net>
2025-04-12 21:41:48 +0000

    enabled cors for localhost ip

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, origins=["http://localhost:3000", "https://wecanhack.com"])
+CORS(app, origins=["http://localhost:3000", "https://wecanhack.com", "https://127.0.0.1:3000"])
 
 @app.route('/health-check', methods=["GET"])
 def health_check():
