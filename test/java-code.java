638a67f8d351a044124322eff54d6bc4c96969ad
Seriously Mac <seriously-mac@Seriouslys-MacBook-Air.local>
2025-04-14 02:26:48 -0500

    update test executor

---
+++ test/java-code.java
@@ -0,0 +1,16 @@
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result= {5,6};
+        return result;
+        
+    }
+}
+
+
+public class Main{
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        solution.twoSum(new int[]{1,2,3}, 4)
+    }
+}
